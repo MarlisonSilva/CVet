c3f99ae9ddb97c6e6c3a5587a351b9c6411ebde7
Marlison Silva <marlisonsoaresjp@gmail.com>
2023-09-30 08:18:46 -0300

    feat: arquivo com validações

---
+++ utils.c
@@ -0,0 +1,33 @@
+// Validations:
+// [ ] Nome
+// [ ] CPF
+// [ ] E-mail
+// [ ] Data
+// [ ] Telefone
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+
+#define true 1
+#define false 0
+
+int validate_name(char name[]) {
+    return false; 
+}
+
+int validate_cpf() {
+    return false;
+}
+
+int validate_email() {
+    return false;
+}
+
+int validate_date() {
+    return false;
+}
+
+int validate_phone() {
+    return false;
+}
---
+++ utils.h
@@ -0,0 +1,11 @@
+// Validations
+// [ ] Nome
+// [ ] CPF
+// [ ] E-mail
+// [ ] Data
+// [ ] Telefone
+int validate_name();
+int validate_cpf();
+int validate_email();
+int validate_date();
+int validate_phone();
